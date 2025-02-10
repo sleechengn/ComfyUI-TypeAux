41a36260e1dd46ded56297c4fa71b7494fba2f1f
alicia <sleechengn@live.cn>
2025-02-10 03:20:29 +0000

    修正实事错误

--- nodes/StringReplace.py
+++ nodes/StringReplace.py
@@ -7,9 +7,9 @@ class StringReplace:
     def INPUT_TYPES(s):
         return {
             "required": {
-                "text": ("STRING",{"defaultInput": True, "default": "{}", "multiline": True}),
-                "match": ("STRING",{"defaultInput": True, "default": "{}", "multiline": True}),
-                "value": ("STRING",{"defaultInput": True, "default": "", "dynamicPrompts": True,"multiline": True}),
+                "text": ("STRING",{"defaultInput": True, "default": "", "multiline": True}),
+                "match": ("STRING",{"defaultInput": False, "default": "\\<think\\>.*\\<\\/think\\>\\n+", "multiline": True}),
+                "value": ("STRING",{"defaultInput": False, "default": "", "dynamicPrompts": True,"multiline": True}),
                 "match_type": (["text", "regex"], {"default": "regex"}),
             }
         }
@@ -24,4 +24,4 @@ class StringReplace:
             pattern = re.compile(match,re.DOTALL)
             return (re.sub(pattern, value, text),)
         if match_type == "text":
-            return text.replace(match,value)
+            return (text.replace(match,value),)
