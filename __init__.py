7d8325f0c2e83ea4b6e85b2a3f8e6d8cbe83680c
alicia <sleechengn@live.cn>
2025-05-20 23:49:54 +0000

    保存程序

--- __init__.py
+++ __init__.py
@@ -2,6 +2,6 @@ from .nodes.PromptTextNode import PromptTextNode
 from .nodes.StringReplace import StringReplace
 
 NODE_CLASS_MAPPINGS = {
-        "PromptTextNode" : PromptTextNode,
-        "StringReplace" : StringReplace
+        "PromptTextNodeFunction" : PromptTextNode,
+        "StringReplaceFunction" : StringReplace
 }
